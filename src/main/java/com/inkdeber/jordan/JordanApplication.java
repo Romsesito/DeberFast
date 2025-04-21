1d60a4727ad24b35b2a91a6899da9c26f9d468c2
Romsesito <graphicsrom@gmail.com>
2025-04-21 13:13:28 -0500

    Start

---
+++ src/main/java/com/inkdeber/jordan/JordanApplication.java
@@ -0,0 +1,13 @@
+package com.inkdeber.jordan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JordanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JordanApplication.class, args);
+	}
+
+}
